c3ff9690ce92a5ada2e4c63aa85c74100bbfb571
양지윤 <yangjiyun748@gmail.com>
2025-08-27 02:45:00 +0900

    마이 페이지 최종 수정

--- src/main/java/com/hongchelin/mypage/controller/BadgeController.java
+++ src/main/java/com/hongchelin/mypage/controller/BadgeController.java
@@ -7,7 +7,7 @@ import com.hongchelin.mypage.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//
 // BadgeController.java
 @RestController
 @RequestMapping("/api")
